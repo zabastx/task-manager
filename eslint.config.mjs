8e50a3550e70a57c0c090005d22731543d14a964
zabastx <zabastx@gmail.com>
2025-07-12 16:32:18 +0300

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,10 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
 	rules: {
 		'@typescript-eslint/no-explicit-any': 'off',
-		'@stylistic/comma-dangle': 'off'
+		'@stylistic/comma-dangle': 'off',
+		'@stylistic/indent': ['error', 'tab'],
+		'@stylistic/indent-binary-ops': ['error', 'tab'],
+		'@stylistic/no-tabs': 'off',
+		'vue/html-indent': 'off'
 	}
 })
